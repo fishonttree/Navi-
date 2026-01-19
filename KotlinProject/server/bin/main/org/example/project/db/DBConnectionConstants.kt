8c2347e81e08c56c21cc387437969947ad9aa336
Fish <116611743+fishonttree@users.noreply.github.com>
2026-01-19 14:36:17 -0500

    Modified Docker Postgres script

--- KotlinProject/server/bin/main/org/example/project/db/DBConnectionConstants.kt
+++ KotlinProject/server/bin/main/org/example/project/db/DBConnectionConstants.kt
@@ -2,7 +2,7 @@ package org.example.project.db
 
 object DBConnectionConstants {
     const val POSTGRES_DB_HOST = "localhost"
-    const val POSTGRES_DB_PORT = 5432
+    const val POSTGRES_DB_PORT = 5433
     const val POSTGRES_DB_NAME = "navi_db"
     const val POSTGRES_USER_NAME = "postgres"
     const val POSTGRES_USER_PSW = "postgres"
